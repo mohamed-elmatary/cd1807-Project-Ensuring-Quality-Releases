f50506d066a1418ee8b7e8680ce88ec928704dc1
msmahmoud <mohamedelmatary2018@gmail.com>
2025-05-20 18:33:12 +0300

    merge

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -30,7 +30,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 }
 
 resource "azurerm_virtual_machine_extension" "monitor_agent" {
-  name                 = "AzureMonitorLinuxAgent"
+  name                       = "${azurerm_linux_virtual_machine.vm.name}-AzureMonitorLinuxAgent"
   virtual_machine_id   = azurerm_linux_virtual_machine.vm.id
   publisher            = "Microsoft.Azure.Monitor"
   type                 = "AzureMonitorLinuxAgent"
