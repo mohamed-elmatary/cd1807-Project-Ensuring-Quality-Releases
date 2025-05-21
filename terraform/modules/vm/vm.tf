ccf29d7691bb50143f9c26abac9ddff84ac6a0bf
msmahmoud <mohamedelmatary2018@gmail.com>
2025-05-21 03:01:03 +0300

    merge

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,7 +29,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "20_04-lts"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
