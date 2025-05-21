c1cb60e88f3eaeceddb8188fc1b83ce966d7b56c
msmahmoud <mohamedelmatary2018@gmail.com>
2025-05-21 04:38:42 +0300

    merge

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,7 +20,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [azurerm_network_interface.nic.id]
   admin_ssh_key {
     username   = "azureuser"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEl15mo0lKhiR6j11DltA+Tjsp9Tg7rc0YDQR2Y4yEOPy1/QFzza0CuVuwKAHCf6Q172z4+R1jfqDEWHvKNDXt3P1Tbi8E01KG4HbC2uYgdnYRuVVjen6JUxFhv1UXyynISz5wyOHoXrLxefRGb2uizOKKlUyaw3DWAGzWdx1dcrXJP8UtFf+ZUNhmIz6VVvErDjZPqhaHWnjbIe6QblI7GQO9SJH6kJgC/znRsowdF27CBIUvCbu2FXQEB4wdgHsIVOXbslNZou4nJIo17lgm61CUkLcX1zJmTE5avnjI8awrM/xeWin8keRtLAFI62nw14H7vcQH2uunbZVnJs0Ub+Q13+FOAeJU940QZDUxwBkZBdKHjqBH+I5OeRg4SxU5aLIkM55rfC2+EJSYGe/7SlRKPzzcS5Jkbwgx+UPkR39o8+/9XXBCpjFCd1jeA3urpbtgJfDlVF4P7og5vQ0SHTsKkPlGIfQdW2R6B5X15YOR1CjtNGXbVwAAoQJRr7k= MohamedShaban@MohamedShaaban"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDePWJKqOVbwDg1OOwdYhWrW9R+TihNQebU0PZK9rUH+gvfGarxWi9ri5qa+J09JqvmfHLfKv5aVBrgk01lk/kIWNGexnfAyu6uKjkvVvOnTirf7hjgRj9tIBaSssRrkoTJx8/K7BE5tsI0Iey/e/zZC/ij98lGO6MXQMcYdb6+OKV1kvKfnOFoOIA65+IbCNoqv63SyySMkdO/C+mXvM1AYTfD48tj2SIHImDZ2SrhzKednfviu19Tdbh6E5z/erC1epPiRBDLPSU4zXYIO+8vZ0JiJZyS+SHwDze2ip9mkYuXMrGZFf1/i5TGXkG1VlPeRJ+ffUJbn2nQPGBIyRJyQx4x4mYAiRVBfMq4HMFm/ZktN+CBKznHItbhe2hATAzuj3sgCuE8hTlLyYGLTotQCppfOlHqbda5zZUZ9B0U16CV8hHexvioNqbD3Lky2kGp7vvaCRXIP/BuXYR7PystNyusvfpSrP2DClTjdYTJO7XHVrDMLnvsekBIzikRmhc= mohamed@SandboxHost-638833790602742044"
   }
   os_disk {
     caching           = "ReadWrite"
