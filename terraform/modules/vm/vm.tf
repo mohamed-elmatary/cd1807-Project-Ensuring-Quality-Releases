0fbb4e7ecf8a88f4709ba5096e637f499e01f91d
msmahmoud <mohamedelmatary2018@gmail.com>
2025-05-21 10:12:30 +0300

    merge

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -27,9 +27,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
   source_image_id = "/subscriptions/aa9cd12c-7c25-41fa-b40b-6650c05128ba/resourceGroups/AZUREDEVOPS2/providers/Microsoft.Compute/images/myApplication714-VM-image-20250521052058"
-  
-  lifecycle {
-    ignore_changes = [ AzureMonitorLinuxAgent ]
+ lifecycle {
+    ignore_changes = ["*"]
   }
 }
 
