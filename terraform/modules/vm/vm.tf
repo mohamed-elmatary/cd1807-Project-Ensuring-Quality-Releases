6d6e941884357c47a72b7f38a64d37af56972220
msmahmoud <mohamedelmatary2018@gmail.com>
2025-05-21 09:39:17 +0300

    merge

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -31,18 +31,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
   allow_extension_operations = true
 }
 
-resource "azurerm_virtual_machine_extension" "azure_monitor_linux_agent" {
-  name                 = "AzureMonitorLinuxAgent"
-  virtual_machine_id   = azurerm_linux_virtual_machine.vm.id
-  publisher            = "Microsoft.Azure.Monitor"
-  type                 = "AzureMonitorLinuxAgent"
-  type_handler_version = "1.35"
-  auto_upgrade_minor_version = true
-  automatic_upgrade_enabled = true
-  settings = jsonencode({
-    GCS_AUTO_CONFIG           = true
-    azureResourceId           = azurerm_linux_virtual_machine.vm.id
-  })
-}
-
 
