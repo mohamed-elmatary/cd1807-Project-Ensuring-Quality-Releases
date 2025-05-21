7b78e5fff2aea9f91ed5e04f74f91f9a42e9f3e4
msmahmoud <mohamedelmatary2018@gmail.com>
2025-05-21 05:24:01 +0300

    merge

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -26,11 +26,5 @@ resource "azurerm_linux_virtual_machine" "vm" {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
- source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  
+  source_image_id = "/subscriptions/aa9cd12c-7c25-41fa-b40b-6650c05128ba/resourceGroups/AZUREDEVOPS2/providers/Microsoft.Compute/images/myApplication714-VM-image-20250521052058"
 }
